9e6f05c4c11095ac0d684d5553ae342f18e000fb
mautomic <maurya373@gmail.com>
2021-01-15 19:08:36 -0600

    add new release github action

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -7,14 +7,12 @@ fun main(args: Array<String>) {
     Sentry.init {
         it.dsn = args[0]
         it.environment = "QA"
-        it.release = "0.1.1"
+        it.release = "0.1.2"
     }
-    startApp("Different Test Message")
+    startApp("Latest Test Message")
 }
 
 fun startApp(message: String) {
-
-    Sentry.captureMessage("Started application successfully", SentryLevel.INFO)
 
     try {
         throw IllegalAccessException(message)
