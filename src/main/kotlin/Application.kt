6d462363711b12546fd8884e7b251c9569245ac5
mautomic <maurya373@gmail.com>
2021-01-18 15:48:47 -0600

    finish basic release deployment closes #1

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,21 +1,20 @@
 import io.sentry.Sentry
-import java.lang.IllegalStateException
 
 fun main(args: Array<String>) {
 
     Sentry.init {
         it.dsn = if (args.size > 0) args[0] else ""
         it.environment = "QA"
-        it.release = "v0.1.3"
+        it.release = "v0.1.4"
     }
-    startApp("Another Test Message")
+    startApp("Secondary Test Message")
 }
 
 fun startApp(message: String) {
 
     try {
-        throw IllegalStateException(message)
-    } catch (e: IllegalStateException) {
+        throw IllegalCallerException(message)
+    } catch (e: IllegalCallerException) {
         Sentry.captureException(e)
     }
 }
